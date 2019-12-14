e27ec5f1dd0270bbde0b02bb4fde657e76443292
Chandra Sekhar Nayak <chansek@live.com>
2019-12-14 18:03:07 +0530

    Local functions access from other class

---
+++ kotlin/src/com/chanse/code/functions/LocalFunctions.java
@@ -0,0 +1,9 @@
+package com.chanse.code.functions;
+
+public class LocalFunctions {
+
+    public static void main(String[] args) {
+        boolean validate = LocalFunctionsKt$login$1.INSTANCE.invoke("103");
+        System.out.println(validate);
+    }
+}
